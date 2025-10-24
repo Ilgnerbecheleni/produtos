f5fbdd968b1d4385237ab36cad6ff989a98a891b
Ilgner Aparecido Becheleni <81272773+Ilgnerbecheleni@users.noreply.github.com>
2025-10-24 16:13:57 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+// ======= CONFIGURE AQUI =======
+ const SUPABASE_URL = "https://pnhnriblgmmcfumgksyf.supabase.co";
+ const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBuaG5yaWJsZ21tY2Z1bWdrc3lmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEzMDY1MTUsImV4cCI6MjA3Njg4MjUxNX0.YVtJGvmV97UfbgqDwcDQBZ8fUjRGTOkBVIzy1AS_fmU";
+// ==============================
+
+
+const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
